0e418c7751f6b7f9cf1b3adc5bf6ddcfa4e8bd5f
TapzQ <109795631+TapzQ@users.noreply.github.com>
2023-01-26 15:47:58 +0100

    Add files via upload

---
+++ file.py
@@ -0,0 +1,29 @@
+
+from colorama import Fore
+import time
+import secrets
+from random import randint
+
+continuing = True
+
+btcval = 14404.41 # Update die Nummer wenn der Kurs hoch oder runter geht. 
+
+while True:
+  if continuing == True:
+    time.sleep(.01)
+    ranInt = randint(0, 2500)
+    if ranInt <= 1:
+      randomBTC = randint(1,100)/100
+      print(Fore.WHITE + "> 0x" + secrets.token_hex(20) + Fore.GREEN + " > " + str(randomBTC) + " BTC ($" + str("{:,}".format(round(btcval*randomBTC,2))) + ")")
+      answer = input("> Would you like to continue? (Y/N) -> ")
+      if answer.lower() == "y":
+        continuing = True
+      else:
+       continuing = False
+    else:
+      print(Fore.WHITE + "> 0x" + secrets.token_hex(20) + Fore.RED + " > 0.00 BTC ($0.00")
+  else:
+    break
+      
+  
+      
