c2a33a0d236b5faccd3eb26c2c86233b0c861eb2
Tapz <109795631+Tapz1337@users.noreply.github.com>
2025-02-27 11:17:39 +0100

    Update file.py
    // Added a new print statement after finding BTC that says the amount has been imported to your crypto account
    
    // Updated the value of btc 
    
    // Change the colour after finding bitcoins

--- file.py
+++ file.py
@@ -1,4 +1,3 @@
-
 from colorama import Fore
 import time
 import secrets
@@ -6,7 +5,7 @@ from random import randint
 
 continuing = True
 
-btcval = 14404.41 # Update die Nummer wenn der Kurs hoch oder runter geht. 
+btcval = 82.393 # Update die Nummer wenn der Kurs hoch oder runter geht. 
 
 while True:
   if continuing == True:
@@ -14,16 +13,14 @@ while True:
     ranInt = randint(0, 2500)
     if ranInt <= 1:
       randomBTC = randint(1,100)/100
-      print(Fore.WHITE + "> 0x" + secrets.token_hex(20) + Fore.GREEN + " > " + str(randomBTC) + " BTC ($" + str("{:,}".format(round(btcval*randomBTC,2))) + ")")
+      print(Fore.GREEN + "> 0x" + secrets.token_hex(20) + Fore.GREEN + " > " + str(randomBTC) + " BTC ($" + str("{:,}".format(round(btcval*randomBTC,2))) + ")")
+      print(Fore.GREEN + "> SUCCESS! " + str(randomBTC) + " BTC has been imported to your crypto account!")
       answer = input("> Would you like to continue? (Y/N) -> ")
       if answer.lower() == "y":
         continuing = True
       else:
-       continuing = False
+        continuing = False
     else:
-      print(Fore.WHITE + "> 0x" + secrets.token_hex(20) + Fore.RED + " > 0.00 BTC ($0.00")
+      print(Fore.LIGHTMAGENTA_EX + "> 0x" + secrets.token_hex(20) + Fore.RED + " > 0.00 BTC ($0.00)")
   else:
     break
-      
-  
-      
